66888657d1f8dcfc2825b8ae983d49845353edbe
420anshulkumar8-lab <420anshulkumar8@gmail.com>
2026-06-20 04:37:21 -0700

    Add files via upload

--- lib/config/remote_config.dart
+++ lib/config/remote_config.dart
@@ -2,7 +2,7 @@
 
 class RemoteConfig {
   static const String configUrl =
-      'https://gist.githubusercontent.com/420anshulkumar8-lab/0c2fea6e262cf353543414f68b9c9b74/raw/d7b86cb89c42d79f40b56256ab40993fd743da33/config.json';
+      'https://gist.githubusercontent.com/420anshulkumar8-lab/0c2fea6e262cf353543414f68b9c9b74/raw/config.json';
 
   static const String defaultModel = 'meta-llama/llama-3.3-70b-instruct:free';
 
